601ce0c30c575346389282991ec45a43e33e9bc8
Kazim Naqvi <kazim@Kazims-MacBook-Air.local>
2022-01-26 16:18:07 -0500

    add handlebars implementation for v2

--- v2/src/app.ts
+++ v2/src/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
+import { engine } from "express-handlebars";
 import cors from "cors";
 import dotenv from "dotenv";
+import aboutMe from "./data/aboutMe";
 
 dotenv.config();
 const port = process.env.PORT;
@@ -14,7 +16,25 @@ const corsOptions = {
 
 const app = express();
 
+app.use("/styles", express.static("styles"));
+app.use("/media", express.static("media"));
+
+app.engine(
+  "hbs",
+  engine({
+    defaultLayout: "main",
+    extname: ".hbs",
+    layoutsDir: "./views/layouts",
+    partialsDir: "./views/partials",
+  })
+);
+
+app.set("view engine", "hbs");
 app.use(cors(corsOptions));
+
+app.get("/", (req, res) => {
+  res.render("resume", { aboutMe, title: aboutMe.title });
+});
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
---
+++ v2/src/data/aboutMe.ts
@@ -0,0 +1,89 @@
+export default {
+  title: "Kazim Naqvi",
+  careerBio: {
+    name: "Kazim Naqvi",
+    status: "Incoming Software Engineer @ HubSpot.",
+  },
+
+  gitURL: "https://github.com/smknaqvi",
+  linkedinURL: "https://linkedin.com/in/smknaqvi",
+  mailURL: "mailto:k.naqvi@mail.utoronto.ca",
+
+  about:
+    "Hi there, I'm Kazim! I'm a Software Developer and a novice photographer on the side",
+  education: [
+    {
+      school: "University of Toronto",
+      studyType: "Honours Bachelor of Science (Computer Science - Co-op)",
+      timeline: "2017 - 2022 (Expected Graduation: April 2022)",
+      description: "GPA: 3.54/40",
+    },
+  ],
+  experience: [
+    {
+      title: "Software Engineer (Intern)",
+      org: "HubSpot",
+      location: "Cambridge, MA, United States",
+      timeline: "(1 yr.) Jan. 2020 – Aug. 2020 and June 2021 – Aug. 2021",
+      skills:
+        "Tools: TypeScript, Javascript, React, Flux, Redux, Selenium, Mocha, Jasmine, Enzyme",
+      details: [
+        "Engineered front end for custom call outcomes, a highly requested calling feature from customers",
+        "Delivered new register phone number flow, resulting in a 50% decrease in related support tickets",
+        "Designed and developed a call history search tool enabling support to diagnose customer problems",
+        "Implemented parts of the new drag and drop calling widget redesign used by thousands of customers",
+        "Engineered a sandbox environment to develop, test, and compare different rich text editor libraries",
+        "Architected acceptance tests using page object model design enabling automated end to end testing of a rich text editor used across the HubSpot app",
+        "Triaged rich text support tickets by communicating with support, investigating, and developing changes",
+      ],
+    },
+    {
+      title: "Full Stack Developer (Intern)",
+      org: "University of Toronto, The BRIDGE",
+      location: "Toronto, ON, Canada",
+      timeline: "(1 yr.) May 2019 – Dec. 2019 and Jan. 2021 – Mar. 2021",
+      skills: "Tools: JavaScript, Express, MySQL, Docker, Nginx, React",
+      details: [
+        "Implemented end to end asymmetric encryption of sensitive immigrant data",
+        "Deployed an on-premises instance of Sentry enabling developers to diagnose bugs efficiently",
+        "Engineered parts of a deployment pipeline to automate the continuous deployment of code changes",
+        "Engineered an extensible data parser that can be configured to collect immigrant data from Excel files",
+      ],
+    },
+    {
+      title: "Business Intelligence Developer (Intern)",
+      org: "Ontario Shores",
+      location: "Toronto, ON, Canada",
+      timeline: "(1 yr.) May 2019 – Dec. 2019 and Jan. 2021 – Mar. 2021",
+      skills:
+        "Tools: SQL Server, SQL Server Data Tools (Analysis/Integration/Reporting), Power BI, MDX, DAX",
+      details: [
+        "Developed a self-serving data model with OLAP cube to enable quick reporting and analysis, saving the finance department over 60 hours per month",
+        "Developed objects such as tables, stored procedures, and an analysis database enabling analysts to build business intelligence reports",
+      ],
+    },
+  ],
+  projects: [
+    {
+      name: "YouTube Party",
+      repoURL: "https://github.com/smknaqvi/project-smol-brain",
+      skills:
+        "Tools: TypeScript, React, Express, Socket.io, MongoDB, Redis, Docker, Nginx",
+      description:
+        "Worked in a team of three developers to create a web app for watching YouTube videos synchronously and chat with friends across different locations.",
+    },
+    {
+      name: "Personal Website",
+      skills: "TypeScript, Node.js, Express, Handlebars, HTML, CSS",
+      description:
+        "A simple personal website for showing my experience and projects.",
+    },
+    {
+      name: "SportCred",
+      repoURL: "https://github.com/smknaqvi/project-todo",
+      skills: "Tools: JavaScript, React, Redux, Express, MongoDB",
+      description:
+        "Worked in a team of seven to create a sports social media app for sports fans and analysts. Users can create posts, share analysis, debate, and play trivia games on the platform.",
+    },
+  ],
+};
